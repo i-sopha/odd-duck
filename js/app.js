52633077dbeae88c44a8f09e04ecb7693a0b2767
Isabel Sopha <i.sopha94@gmail.com>
2023-03-20 05:44:30 -0700

    updated odd ducks

--- js/app.js
+++ js/app.js
@@ -1,1 +1,108 @@
+'use strict';
 
+const state = [];
+let roundsOfVoting = 25;
+
+function Image(name, source) {
+  this.name = name;
+  this.source = source;
+  this.timesClicked = 0;
+  this.timesShown = 0;
+}
+
+state.push(new Image('bag', 'img/bag.jpg'));
+state.push(new Image('banana', 'img/banana.jpg'));
+state.push(new Image('bathroom', 'img/bathroom.jpg'));
+state.push(new Image('boots', 'img/boots.jpg'));
+state.push(new Image('breakfast', 'img/breakfast.jpg'));
+state.push(new Image('bubblegum', 'img/bubblegum.jpg'));
+state.push(new Image('chair', 'img/chair.jpg'));
+state.push(new Image('cthulhu', 'img/cthulhu.jpg'));
+state.push(new Image('dog duck', 'img/dog-duck.jpg'));
+state.push(new Image('dragon', 'img/dragon.jpg'));
+state.push(new Image('pen', 'img/pen.jpg'));
+state.push(new Image('pet sweep', 'img/pet-sweep.jpg'));
+state.push(new Image('scissors', 'img/scissors.jpg'));
+state.push(new Image('shark', 'img/shark.jpg'));
+state.push(new Image('sweep', 'img/sweep.png'));
+state.push(new Image('tauntaun', 'img/tauntaun.jpg'));
+state.push(new Image('unicorn', 'img/unicorn.jpg'));
+state.push(new Image('water can', 'img/water-can.jpg'));
+state.push(new Image('wine glass', 'img/wine-glass.jpg'));
+
+let imgEls = document.querySelectorAll('img');
+let voteTrackerEl = document.getElementById('vote-tracker');
+
+console.log('CURRENTLY RENDERED IMAGES', imgEls);
+console.log('CURRENT STATE', state);
+
+// Render images
+// imgEls[0].src = state[0].source;
+// imgEls[0].id = state[0].name;
+// imgEls[1].src = state[1].source;
+// imgEls[1].id = state[1].name;
+// imgEls[2].src = state[2].source;
+// imgEls[2].id = state[2].name;
+renderImgs();
+
+function generateRandomImage() {
+  return Math.floor(Math.random() * state.length);
+}
+
+function renderImgs() {
+  let img1 = state[generateRandomImage()];
+  let img2 = state[generateRandomImage()];
+  let img3 = state[generateRandomImage()];
+  while (img1.name === img2.name || img1.name === img3.name || img2.name === img3.name) {
+    img3 = state[generateRandomImage()];
+  }
+
+  imgEls[0].src = img1.source;
+  imgEls[0].id = img1.name;
+  img1.timesShown += 1;
+  imgEls[1].src = img2.source;
+  imgEls[1].id = img2.name;
+  img2.timesShown += 1;
+  imgEls[2].src = img3.source;
+  imgEls[2].id = img3.name;
+  img3.timesShown += 1;
+
+}
+
+function handleImgClick(event) {
+  console.log(event.target);
+
+  let imgThatWasClicked = event.target.id;
+  state.forEach(image => {
+    if (image.name === imgThatWasClicked) {
+      image.timesClicked += 1;
+    }
+  });
+  console.log('UPDATED STATE', state);
+
+  if (roundsOfVoting) {
+    renderImgs();
+    roundsOfVoting--;
+  } else {
+    voteTrackerEl.removeEventListener('click', handleImgClick);
+  }
+
+};
+
+voteTrackerEl.addEventListener('click', handleImgClick);
+
+
+
+function showResults() {
+    let resultsEl = document.getElementById('results');
+    resultsEl.innerHTML = '';
+
+    for (let i = 0; i < state.length; i++) {
+        let liEl = document.createElement('li');
+        liEl.textContent = `${state[i].name} had ${state[i].timesClicked} votes, and was seen ${state[i].timesShown} times.`;
+        resultsEl.appendChild(liEl);
+    }
+}
+
+let resultsButton = document.getElementById('show-results');
+resultsButton.addEventListener('click', showResults);
